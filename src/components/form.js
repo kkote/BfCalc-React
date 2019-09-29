82bc01d277ed08eac2a08b94de109a3f4b21a9be
kkote <kaygray521@gmail.com>
2019-09-29 00:58:04 -0500

    Height in Inches

--- src/components/form.js
+++ src/components/form.js
@@ -10,17 +10,21 @@ class Form extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
     const data = new FormData(event.target);
-    console.log(data)
+    console.log(data);
     const stringdata = stringifyFormData(data);
-    console.log(stringdata)
-    const datadata = JSON.parse(stringdata);
-    const bmi = ((datadata.weight / 3969) * 703).toPrecision(3);
+    console.log(stringdata);
+    const dataP = JSON.parse(stringdata);
+    const heightInche = ((dataP.feet * 12)+(dataP.inches * 1));
+    
+    const bmi = ((dataP.weight / 3969) * 703).toPrecision(3);
+    console.log(bmi)
     
     this.setState({
       res: stringdata,
-      data: datadata,
-      one: datadata.age*datadata.weight,
+      data: dataP,
+      one: dataP.age*dataP.weight,
       bmi: bmi,
+      heightInches: heightInche
     });
     
     // fetch('/api/form-submit-url', {
@@ -66,14 +70,14 @@ class Form extends React.Component {
           <div className="inputRow basicsRow basicsRowCombo">
 
             <label htmlFor="age">Age
-             <input id="age" name="age" type="number" defaultValue={28} required />
+             <input id="age" name="age" type="number" defaultValue={28} min="18" max="99" required />
             </label>
           </div>
 
           <div className="inputRow basicsRow basicsRowCombo">
 
             <label htmlFor="weight">Weight
-             <input id="weight" name="weight" type="number" defaultValue={115} required />
+             <input id="weight" name="weight" type="number" defaultValue={115} min="85" max="600" required />
             </label>
 
           </div>
@@ -84,12 +88,12 @@ class Form extends React.Component {
 
             <div className=" basicsRow heightRow">
               <label htmlFor="feet">
-                <input id="feet" name="feet" type="number" defaultValue={5} placeholder='Feet' required />
+                <input id="feet" name="feet" type="number" defaultValue={5} placeholder='Feet' min="4" max="7" required />
                 <span className="heightSpan">Feet</span>
               </label>
 
               <label htmlFor="inches">
-                <input id="inches" name="inches" type="number" defaultValue={3} placeholder='Inches' required />
+                <input id="inches" name="inches" type="number" defaultValue={3} placeholder='Inches'  min="0" max="11" required />
                 <span className="heightSpan">Inches</span>
               </label>
             </div>
@@ -102,15 +106,15 @@ class Form extends React.Component {
               <legend>Measurements (Inches)</legend>
 
               <label htmlFor="hips">Hips
-               <input id="hips" name="hips" type="number" defaultValue={34} required />
+               <input id="hips" name="hips" type="number" defaultValue={34} min="25" max="60" required />
               </label>
 
               <label htmlFor="waist">Waist
-               <input id="waist" name="waist" type="number" defaultValue={25} required />
+               <input id="waist" name="waist" type="number" defaultValue={25} min="18" max="50" required />
               </label>
 
               <label htmlFor="neck">Neck
-               <input id="neck" name="neck" type="number" defaultValue={12} required />
+               <input id="neck" name="neck" type="number" defaultValue={12} min="10" max="26" required />
               </label>
 
             </fieldset>
@@ -151,6 +155,12 @@ class Form extends React.Component {
             <div className="res-block">
             <h3>Data to be sent:</h3>
             <pre>FormData {this.state.res}</pre>
+            </div>
+        )}
+          {this.state.heightInches && (
+            <div className="res-block">
+            <h3>heightInches:</h3>
+            <pre>{this.state.heightInches}</pre>
             </div>
         )}
        
