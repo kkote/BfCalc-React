5f138d0a3ff0afba8f5100d43fcb9a7312c61784
kkote <kaygray521@gmail.com>
2019-10-01 13:17:40 -0500

    Form CSS

--- src/components/form.js
+++ src/components/form.js
@@ -49,239 +49,167 @@ class CalcForm extends React.Component {
         <Col className="calcForm">
           <header>
             <h4>Your Measurements</h4>
-        </header>
-        <hr></hr>
-        <Form onSubmit={this.handleSubmit}>
-
-        <FormGroup className="inputRow">
-
-          <Label htmlFor="date"> Date
-          <Input  id="date" name="date" type="date" defaultValue={'2019-09-11'} required />
-          </Label>
-          </FormGroup>
+          </header>
+          <hr></hr>
+          <Form onSubmit={this.handleSubmit}>
 
 
-      {/*}    <div className="inputRow">
-            <label htmlFor="date"> Date
-           <input id="date" name="date" type="date" defaultValue={'2019-09-11'} required />
-            </label>
-          </div>  
+            <Row form>
+              <Col >
 
-      
+                <FormGroup tag="fieldset" row>
+                  <legend className="col-form-label col-sm-2">Gender</legend>
 
-          <div className="inputRow">
-            <fieldset >
-              <legend>Gender</legend>
+                  <FormGroup check>
+                    <Label check>
+                      <Input id="female" name="gender" type="radio" value={'female'} />{' '}
+                      Female
+                    </Label>
+                  </FormGroup>
+                  
+                  <FormGroup check>
+                    <Label check>
+                      <Input id="male" name="gender" type="radio" value={'male'} required />{' '}
+                      Male
+                    </Label>
+                  </FormGroup>
+                </FormGroup>
+              </Col>
 
-              <label htmlFor="female">
-                <input id="female" name="gender" type="radio" value={'female'} />
-                Female</label>
-
-              <label htmlFor="male">
-                <input id="male" name="gender" type="radio" value={'male'} required />
-                Male</label>
-
-            </fieldset>
-          </div>*/}
+              <Col>
+                <Label htmlFor="date"> Date
+                  <Input id="date" name="date" type="date" defaultValue={'2019-09-11'} required />
+                </Label>
+              </Col>
+            </Row>
 
 
-          <FormGroup tag="fieldset" row>
-          <legend className="col-form-label col-sm-2">Gender</legend>
-         
-            <FormGroup check>
-              <Label check>
-                <Input  id="female" name="gender" type="radio" value={'female'} />{' '}
-                Female
-              </Label>
+            <Row form>
+
+              <Col>
+                <FormGroup className="inputRow basicsRow ">
+                  <Label htmlFor="age"> Age
+                    <Input id="age" name="age" type="number" defaultValue={28} min="18" max="99" required />
+                  </Label>
+                </FormGroup>
+              </Col>
+
+              <Col>
+                <Label htmlFor="weight"> Weight
+                  <Input name="weight" type="number" defaultValue={115} min="85" max="600" required />
+                </Label>
+              </Col>
+
+            </Row>
+
+
+            <FormGroup >
+              <Label htmlFor="Height">
+                Height </Label>
+              <Row form>
+
+                <Col className="heightRow">
+                  <Label htmlFor="feet">
+                    <Input id="feet" name="feet" type="number" defaultValue={5} placeholder='Feet' min="4" max="7" required />
+                    {/*<span className="heightSpan">Feet</span> */}
+                  </Label>
+                </Col>
+
+                <Col className="heightRow">
+                  <Label htmlFor="inches">
+                    <Input id="inches" name="inches" type="number" defaultValue={3} placeholder='Inches' min="0" max="11" required />
+                   {/*} <span className="heightSpan">Inches</span> */}
+                  </Label>
+                </Col>
+
+              </Row>
+
             </FormGroup>
-            <FormGroup check>
-              <Label check>
-                <Input id="male" name="gender" type="radio" value={'male'} required  />{' '}
-                Male
-              </Label>
+
+          <hr></hr>
+
+            <FormGroup >
+              <Label htmlFor="measurements" className="fieldBold">
+                Measurements (Inches)</Label>
+              {/*} <legend className="col-form-label col-sm-2">Height</legend>  */}
+              <Row form>
+
+                <Col >
+                  <Label htmlFor="hips"> Hips
+                    <Input id="hips" name="hips" type="number" defaultValue={34} min="25" max="60" required />
+                  </Label>
+                </Col>
+
+                <Col >
+                  <Label htmlFor="waist"> Waist
+                    <Input id="waist" name="waist" type="number" defaultValue={25} min="18" max="50" required />
+                  </Label>
+                </Col>
+
+                <Col >
+                  <Label htmlFor="neck"> Neck
+                   <Input id="neck" name="neck" type="number" defaultValue={12} min="10" max="26" required />
+                  </Label>
+                </Col>
+              </Row>
             </FormGroup>
-         
-        </FormGroup>
 
 
-          <FormGroup className="inputRow basicsRow basicsRowCombo">
+            <Row form>
+              <FormGroup tag="fieldset" row>
+                <legend className="col-form-label col-sm-2">Activity Level</legend>
 
-        <Label htmlFor="age"> Age
-        <Input  id="age" name="age" type="number" defaultValue={28} min="18" max="99" required  />
-        </Label>
-        </FormGroup>
+                <FormGroup check>
+                  <Label htmlFor="none" check>
+                    <Input id="none" name="activity" type="radio" value={1.2} required />{' '}
+                    None</Label>
+                </FormGroup>
 
-        {/*}  <div className="inputRow basicsRow basicsRowCombo">
+                <FormGroup check>
+                  <Label htmlFor="light" check>
+                    <Input id="light" name="activity" type="radio" value={1.375} required />{' '}
+                    Light</Label>
+                </FormGroup>
 
-            <label htmlFor="age">Age
-             <input id="age" name="age" type="number" defaultValue={28} min="18" max="99" required />
-            </label>
-          </div>
+                <FormGroup check>
+                  <Label htmlFor="normal" check>
+                    <Input id="normal" name="activity" type="radio" value={1.55} required />{' '}
+                    Normal</Label>
+                </FormGroup>
 
+                <FormGroup check>
+                  <Label htmlFor="extra" check>
+                    <Input id="extra" name="activity" type="radio" value={1.725} required />{' '}
+                    Extra</Label>
+                </FormGroup>
 
-          <div className="inputRow basicsRow basicsRowCombo">
+                <FormGroup check>
+                  <Label htmlFor="heavy" check>
+                    <Input id="heavy" name="activity" type="radio" value={1.9} required />{' '}
+                    Heavy</Label>
+                </FormGroup>
 
-            <label htmlFor="weight">Weight
-             <input id="weight" name="weight" type="number" defaultValue={115} min="85" max="600" required />
-            </label>
+              </FormGroup>
+            </Row>
 
-          </div>
-          */}
+            <button>Calculate </button>
+          </Form>
 
-          <FormGroup className="inputRow">
-
-        <Label htmlFor="weight"> Weight
-        <Input  name="weight" type="number" defaultValue={115} min="85" max="600" required  />
-        </Label>
-        </FormGroup>
-
-        {/* <label className="inputRow basicsRow heightLabel"> Height
-           </label>
-          <div className="inputRow basicsRow heightRowMainDiv">
-
-            <div className=" basicsRow heightRow">
-              <label htmlFor="feet">
-                <input id="feet" name="feet" type="number" defaultValue={5} placeholder='Feet' min="4" max="7" required />
-                <span className="heightSpan">Feet</span>
-              </label>
-
-              <label htmlFor="inches">
-                <input id="inches" name="inches" type="number" defaultValue={3} placeholder='Inches'  min="0" max="11" required />
-                <span className="heightSpan">Inches</span>
-              </label>
-            </div>
-          </div> */}
-          
-
-
-
-
-
-          <Row form>
-          <Label htmlFor="Height"> 
-          Height
-        </Label>
-
-         
-
-          <Col >
-
-        <Label htmlFor="feet">
-        <Input  id="feet" name="feet" type="number" defaultValue={5} placeholder='Feet' min="4" max="7" required />
-        <span className="heightSpan">Feet</span>
-        </Label>
-        </Col>
-
-        <Col >
-
-        <Label htmlFor="inches"> 
-        <Input  id="inches" name="inches" type="number" defaultValue={3} placeholder='Inches'  min="0" max="11" required />
-        <span className="heightSpan">Inches</span>
-        </Label>
-        </Col>
-        
-        </Row>
-
-
-
-
-          <FormGroup tag="fieldset" >
-          <legend className="col-form-label col-sm-2">Measurements(Inches)</legend>
-
-          <FormGroup className="inputRow">
-
-        <Label htmlFor="hips"> Hips
-        <Input  id="hips" name="hips" type="number" defaultValue={34} min="25" max="60" required />
-        </Label>
-        </FormGroup>
-
-        <FormGroup className="inputRow">
-
-        <Label htmlFor="waist"> Waist
-        <Input  id="waist" name="waist" type="number" defaultValue={25} min="18" max="50" required />
-        </Label>
-        </FormGroup>
-
-        <FormGroup className="inputRow">
-
-        <Label htmlFor="neck"> Neck
-        <Input  id="neck" name="neck" type="number" defaultValue={12} min="10" max="26" required  />
-        </Label>
-        </FormGroup>
-        
-        </FormGroup>
-
-
-
-         {/*} <div className="inputRow basicsRow">
-
-            <fieldset>
-              <legend>Measurements (Inches)</legend>
-
-              <label htmlFor="hips">Hips
-               <input id="hips" name="hips" type="number" defaultValue={34} min="25" max="60" required />
-              </label>
-
-              <label htmlFor="waist">Waist
-               <input id="waist" name="waist" type="number" defaultValue={25} min="18" max="50" required />
-              </label>
-
-              <label htmlFor="neck">Neck
-               <input id="neck" name="neck" type="number" defaultValue={12} min="10" max="26" required />
-              </label>
-
-            </fieldset>
-          </div>
-
-          */}
-
-          <div className="inputRow">
-
-            <fieldset >
-              <legend>Activity Level</legend>
-
-              <label htmlFor="none">
-                <input id="none" name="activity" type="radio" value={1.2} required />
-                None</label>
-
-              <label htmlFor="light">
-                <input id="light" name="activity" type="radio" value={1.375} required />
-                Light</label>
-
-              <label htmlFor="normal">
-                <input id="normal" name="activity" type="radio" value={1.55} required />
-                Normal</label>
-
-              <label htmlFor="extra">
-                <input id="extra" name="activity" type="radio" value={1.725} required />
-                Extra</label>
-
-              <label htmlFor="heavy">
-                <input id="heavy" name="activity" type="radio" value={1.9} required />
-                Heavy</label>
-            </fieldset>
-          </div>
-
-          <button>Calculate </button>
-        </Form>
-
-
-       
         </Col>
 
 
-        
+
         <hr></hr>
         <Stats
-           
-            bmi={this.state.bmiDisplay}
-            bf={this.state.bfDisplay}
-            tdee={this.state.tdeeDisplay}
-          />
-         
-       
-       <br></br>
-       
+
+          bmi={this.state.bmiDisplay}
+          bf={this.state.bfDisplay}
+          tdee={this.state.tdeeDisplay}
+        />
+
+
+        <br></br>
+
         {/*{this.state.res && (
             <div className="res-block">
             <h3>Data to be sent:</h3>
@@ -291,7 +219,7 @@ class CalcForm extends React.Component {
 
 
 
-        </Container>
+      </Container>
     );
   }
 }
