af3d0db3984bf7b093c5a7a80eb11d3bfaec07a8
kkote <kaygray521@gmail.com>
2019-11-04 15:57:34 -0500

    Updates

---
+++ precache-manifest.e8a3fd7c7da42c84e1149e8f0ccdb451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f744b01abe65b59e1a02b80ff2df9c03",
+    "url": "/BfCalc-React/index.html"
+  },
+  {
+    "revision": "53d692c8250b725b46a1",
+    "url": "/BfCalc-React/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "71da422bee938115df2c",
+    "url": "/BfCalc-React/static/css/main.54d72895.chunk.css"
+  },
+  {
+    "revision": "53d692c8250b725b46a1",
+    "url": "/BfCalc-React/static/js/2.d313295a.chunk.js"
+  },
+  {
+    "revision": "71da422bee938115df2c",
+    "url": "/BfCalc-React/static/js/main.d48ad10c.chunk.js"
+  },
+  {
+    "revision": "6079c769ca8778b62459",
+    "url": "/BfCalc-React/static/js/runtime~main.270430fc.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/BfCalc-React/precache-manifest.e8a3fd7c7da42c84e1149e8f0ccdb451.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BfCalc-React/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
